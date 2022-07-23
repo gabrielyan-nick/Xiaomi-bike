6780a2519468f894df9f3562c7cafdbd69d4d59a
Nick <junk72305@gmail.com>
2022-07-23 19:11:53 +0300

    commit

---
+++ js/main.js
@@ -0,0 +1,19 @@
+$(function () {
+  $(".bike-slider").slick({
+    arrows: false,
+    dots: true,
+    fade: true,
+    autoplay: true,
+    autoplayspeed: 2000,
+  });
+});
+
+$(function () {
+  $(".slider-slider").slick({
+    arrows: false,
+    dots: true,
+    fade: true,
+    autoplay: true,
+    autoplayspeed: 2000,
+  });
+});
